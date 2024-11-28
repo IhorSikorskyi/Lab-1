7d8001fba2ca1a76bb6cfd848060a61e97473b18
Igor <135720728+IhorSikorskyi@users.noreply.github.com>
2024-11-28 18:21:40 +0200

    6.2

--- CNN.py
+++ CNN.py
@@ -10,30 +10,30 @@ data_path = "flowers"
 
 # Генератор для тренувальних даних з розширенням
 train_datagen = ImageDataGenerator(
-    rescale=1.0/255.0,  # Нормалізація зображень (масштабування пікселів в діапазон [0,1])
-    rotation_range=40,  # Випадкові обертання зображень на 40 градусів
-    width_shift_range=0.2,  # Випадковий зсув зображень по горизонталі
-    height_shift_range=0.2,  # Випадковий зсув зображень по вертикалі
-    shear_range=0.2,  # Випадкові зміщення (shear transformations)
-    zoom_range=0.2,  # Випадкове масштабування
-    horizontal_flip=True,  # Випадковий горизонтальний віддзеркалення
-    fill_mode='nearest',  # Заповнення пікселів після трансформацій
+    rescale=1.0/255.0,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
+)
+
+# Завантаження тренувальних даних з розширенням
+train_data = train_datagen.flow_from_directory(
+    f"{data_path}/train",
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode="categorical",
 )
 
 # Генератор для тестових даних (тільки нормалізація)
 test_datagen = ImageDataGenerator(rescale=1.0/255.0)
 
-# Завантаження тренувальних даних з розширенням
-train_data = train_datagen.flow_from_directory(
-    f"{data_path}/train",  # Папка з тренувальними даними
-    target_size=(128, 128),  # Розмір зображень для масштабування
-    batch_size=32,
-    class_mode="categorical",  # Для багатокласової класифікації
-)
-
 # Завантаження тестових даних (без розширення, тільки нормалізація)
 test_data = test_datagen.flow_from_directory(
-    f"{data_path}/test",  # Папка з тестовими даними
+    f"{data_path}/test",
     target_size=(128, 128),
     batch_size=32,
     class_mode="categorical",
@@ -49,16 +49,12 @@ model.add(Conv2D(128, (3, 3), kernel_regularizer=l2(0.01)))  # L2 регуляр
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128, (3, 3), kernel_regularizer=l2(0.01)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dense(128, activation="relu", kernel_regularizer=l2(0.01)))  # L2 регуляризація
 model.add(Dropout(0.5))  # Dropout для регуляризації
 
-# Останній шар: кількість нейронів = кількість класів (5 для 'daisy', 'dandelion', 'rose', 'sunflower', 'tulip')
-model.add(Dense(5, activation="softmax"))  # Softmax для багатокласової класифікації
+# Останній шар
+model.add(Dense(5, activation="softmax"))
 
 # Компіляція моделі
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
@@ -73,7 +69,7 @@ callbacks = [
 model.fit(
     train_data,
     epochs=10,
-    validation_data=test_data,  # Використовуємо тестові дані для валідації
+    validation_data=test_data,
     callbacks=callbacks
 )
 
